4246a51867c493eeef56df19ac534b23bfefa82a
dbaaz <nfs.sumeet@gmail.com>
2017-03-19 22:06:56 +0530

    Timer working, autorotate breaks it though

--- app/src/main/java/apps/dbaaz/com/vibrateeveryxseconds/MainActivity.java
+++ app/src/main/java/apps/dbaaz/com/vibrateeveryxseconds/MainActivity.java
@@ -1,6 +1,10 @@
 package apps.dbaaz.com.vibrateeveryxseconds;
 
+import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,13 +12,15 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import java.util.Timer;
-
-import static apps.dbaaz.com.vibrateeveryxseconds.MainActivity.state;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     boolean state=false;
     EditText textbox;
     Button btn;
+    int time,temp;
+    Timer t;
+    Vibrator vib;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Timer t = new Timer();
@@ -22,12 +28,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textbox=(EditText)findViewById(R.id.editText);
         btn=(Button)findViewById(R.id.button2);
-
+        vib= (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle b)
+    {
+        b.putBoolean("state",state);
+        t.cancel();
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle b)
+    {
+        if(b.getBoolean("state"))
+        {
+            startTimer();
+            btn.setText("stop");
+        }
     }
 
 
     void vibratePhone()
     {
-
+        vib.vibrate(1000);
     }
+    public void handleButton(View v)
+    {
+        state=!state;
+        if(state) {
+            startTimer();
+            btn.setText("stop");
+        }
+        else
+        {
+            t.cancel();
+            btn.setText("start");
+            textbox.setText(time+"");
+        }
+        textbox.setFocusable(!state);
+        textbox.setClickable(!state);
+        textbox.setFocusableInTouchMode(!state);
+    }
+    void startTimer()
+    {
+        time = Integer.parseInt(textbox.getText().toString());
+        temp = time;
+        t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if(temp>0) {
+                    temp--;
+
+                }
+                else
+                {
+                    vibratePhone();
+                    temp=time;
+                }
+                myhand.obtainMessage(1).sendToTarget();
+            }
+        },0,1000);
+    }
+    public Handler myhand = new Handler() {
+      public void handleMessage(Message msg)
+      {
+        textbox.setText(temp+"");
+      }
+    };
 }
